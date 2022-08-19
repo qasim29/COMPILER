753840897dcddebee6cea65b0ade2c69fb47a6db
qasim.29 <qasimmohammad534@gmail.com>
2022-08-19 20:31:00 +0500

    Initialize Repo

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿using System;
+
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+// var value = System.Environment.GetEnvironmentVariable("dotnet");
+// System.Environment.SetEnvironmentVariable("dotnet" ,value);
