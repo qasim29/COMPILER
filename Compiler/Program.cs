afb01700dc141088fd8b779839a4808f23c5ed6c
qasim.29 <qasimmohammad534@gmail.com>
2022-08-20 16:17:45 +0500

    modified dir struc

--- Compiler/Program.cs
+++ Compiler/Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections;
+
+ArrayList arlist = new ArrayList(); 
+
+// Tokens t = new Tokens("b", "a", 1);
+
+// // Example #1
+// // Read the file as one string.
+// string text = System.IO.File.ReadAllText(@"C:\Users\Public\TestFolder\WriteText.txt");
+
+// // Display the file contents to the console. Variable text is a string.
+// System.Console.WriteLine("Contents of WriteText.txt = {0}", text);
+
+// Example #2
+// Read each line of the file into a string array. Each element
+// of the array is one line of the file.
+string[] lines = System.IO.File.ReadAllLines(@".\res\SOURCE_CODE.txt");
+
+// // Display the file contents by using a foreach loop.
+// System.Console.WriteLine("Contents of WriteLines2.txt = ");
+foreach (string line in lines)
+{
+    // LOGIC FOR CODE CHECKING
+
+}
+
+// // Keep the console window open in debug mode.
+// Console.WriteLine("Press any key to exit.");
+// System.Console.ReadKey();
+
+
+
