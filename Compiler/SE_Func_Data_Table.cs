33936971aacca2572a9c0f207624aa111f4c1e81
qasim.29 <qasimmohammad534@gmail.com>
2022-11-01 17:16:26 +0500

    classs implementation

--- Compiler/SE_Func_Data_Table.cs
+++ Compiler/SE_Func_Data_Table.cs
@@ -1,4 +1,13 @@
 class SE_Func_Data_Table
 {
-    
+    public SE_Func_Data_Table(string name, string type, int scope)
+    {
+        this.name = name;
+        this.type = type;
+        this.scope = scope;
+    }
+
+    public string name { get; set; }
+    public string type { get; set; }
+    public int scope { get; set; }
 }
