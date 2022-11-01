09579869f65d732558b59ddb901a2f79f02e9bb3
qasim.29 <qasimmohammad534@gmail.com>
2022-11-01 16:59:39 +0500

    created a new class for Semantic analyzer

---
+++ Compiler/SE_Class_Data_Table.cs
@@ -0,0 +1,19 @@
+class SE_Class_Data_Table
+{
+    public SE_Class_Data_Table(string name, string type, string am, bool sta, bool final, bool abstrac)
+    {
+        this.name = name;
+        this.type = type;
+        this.am = am;
+        this.sta = sta;
+        this.final = final;
+        this.abstrac = abstrac;
+    }
+    public string name { get; set; }
+    public string type { get; set; }
+    public string am { get; set; }
+    public bool sta { get; set; }
+    public bool final { get; set; }
+    public bool abstrac { get; set; }
+
+}
