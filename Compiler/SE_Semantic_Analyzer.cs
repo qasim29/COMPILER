865c673ba22571a44e30b9cf5ba168dcd5f062dd
qasim.29 <qasimmohammad534@gmail.com>
2022-11-07 13:12:55 +0500

    CHANGED AM

--- Compiler/SE_Semantic_Analyzer.cs
+++ Compiler/SE_Semantic_Analyzer.cs
@@ -3,22 +3,22 @@ class SE_Semantic_Analyzer
 {
     public Dictionary<string, SE_Main_Data_Table> main_table;
     public Dictionary<string, SE_Func_Data_Table> function_table;
-    // public int scope;
-    // public List<int> scopeStack = new List<int>();
-    // public string curr_class_name;
+    public int scope;
+    public List<int> scopeStack = new List<int>();
+    public string curr_class_name;
     public SE_Semantic_Analyzer()
     {
         main_table = new Dictionary<string, SE_Main_Data_Table>();
         function_table = new Dictionary<string, SE_Func_Data_Table>();
     }
-    bool insertMainTable(string name, string type, string tm, string extends)
+    public bool insertMainTable(string name, string type, string tm, string extends)
     {
         if (main_table.ContainsKey(name)) return false;
         main_table[name] = new SE_Main_Data_Table(name, type, tm, extends);
         return true;
 
     }
-    bool insertClassData(string name, string type, string am, bool sta, bool final, bool abstrac, string curr_class_name)
+    public bool insertClassData(string name, string type, string am, bool sta, bool final, bool abstrac, string curr_class_name)
     {
         string key = (name + ":" + type).Split("->")[0];
 
@@ -35,28 +35,28 @@ class SE_Semantic_Analyzer
             return true;
         }
     }
-    bool insertFuncTable(string name, string type, int scope)
+    public bool insertFuncTable(string name, string type, int scope)
     {
         if (function_table.ContainsKey(name)) return false;
         function_table[name + scope.ToString()] = new SE_Func_Data_Table(name, type, scope);
         return true;
     }
-    SE_Main_Data_Table? lookUpMainTable(string name)
+    public SE_Main_Data_Table? lookUpMainTable(string name)
     {
         if (main_table.ContainsKey(name)) return main_table[name];
         else return null;
     }
-    SE_Class_Data_Table? lookUpDataTable(string name, string curr_class_name)
+    public SE_Class_Data_Table? lookUpDataTable(string name, string curr_class_name)
     {
         if (main_table[curr_class_name].cdt.ContainsKey(name)) return main_table[curr_class_name].cdt[name];
         else return null;
     }
-    SE_Class_Data_Table? lookUpDataTable(string name, string pl, string curr_class_name)
+    public SE_Class_Data_Table? lookUpDataTable(string name, string pl, string curr_class_name)
     {
         if (main_table[curr_class_name].cdt.ContainsKey(name + ":" + pl)) return main_table[curr_class_name].cdt[(name + ":" + pl)];
         else return null;
     }
-    string? lookUpFuncTable(string name, List<int> scopeStack, string curr_class_name)
+    public string? lookUpFuncTable(string name, List<int> scopeStack, string curr_class_name)
     {
         for (int i = scopeStack.Count - 1; i >= 0; i--)
         {
@@ -66,7 +66,7 @@ class SE_Semantic_Analyzer
 
         return null;
     }
-    string? compatibility(string left, string right, string op)
+    public string? compatibility(string left, string right, string op)
     {
         if (left == "string" && right == "string")
         {
@@ -81,9 +81,6 @@ class SE_Semantic_Analyzer
         }
         return null;
     }
-    void createScope(int scope, List<int> scopeStack) { scope += 1; scopeStack.Add(scope); }
-    void destroyScope(int scope, List<int> scopeStack) { scopeStack.RemoveAt(scopeStack.Count - 1); }
-
-
-
+    public void createScope() { scope += 1; scopeStack.Add(scope); }
+    public void destroyScope() { scopeStack.RemoveAt(scopeStack.Count - 1); }
 }
