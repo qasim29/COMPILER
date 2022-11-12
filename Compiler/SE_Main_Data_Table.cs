16a8138b1de4b5b02584cd7eeb4e02ee9433cccb
qasim.29 <qasimmohammad534@gmail.com>
2022-11-12 18:09:38 +0500

    generate Tostring method

--- Compiler/SE_Main_Data_Table.cs
+++ Compiler/SE_Main_Data_Table.cs
@@ -15,7 +15,13 @@ class SE_Main_Data_Table
     public string type { get; set; }
     public string tm { get; set; }
     public string extends { get; set; }
-    // public List<SE_Class_Data_Table> cd { get; set; }
     public Dictionary<string, SE_Class_Data_Table> cdt { get; set; }
 
+    // public List<SE_Class_Data_Table> cd { get; set; }
+
+    
+    public override string? ToString()
+    {
+        return "{"+name +","+type +","+tm +","+extends+"}" ;
+    }
 }
