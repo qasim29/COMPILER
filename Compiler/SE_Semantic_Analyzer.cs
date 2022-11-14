91419987843af05ec7a03d3bbdf9fe7c0f138c4f
qasim.29 <qasimmohammad534@gmail.com>
2022-11-14 22:08:01 +0500

    implemented printing functions

--- Compiler/SE_Semantic_Analyzer.cs
+++ Compiler/SE_Semantic_Analyzer.cs
@@ -5,7 +5,7 @@ class SE_Semantic_Analyzer
     public Dictionary<string, SE_Global_Data_Table> global_table;
     public Dictionary<string, SE_Main_Data_Table> main_table;
     public List<int> scopeStack = new List<int>();
-    public string? curr_class_name="";
+    public string? curr_class_name = null;
     public int scope;
     public SE_Semantic_Analyzer()
     {
@@ -17,13 +17,14 @@ class SE_Semantic_Analyzer
     {
         if (global_table.ContainsKey(name)) return false;
         global_table[name] = new SE_Global_Data_Table(name, type, sta, final);
+        printGTable();
         return true;
-
     }
-    public bool insertMainTable(string name, string type, string tm, string extends)
+    public bool insertMainTable(string name, string type, string? tm, string? extends)
     {
         if (main_table.ContainsKey(name)) return false;
         main_table[name] = new SE_Main_Data_Table(name, type, tm, extends);
+        printMainTable();
         return true;
     }
     public bool insertClassData(string name, string type, string am, bool sta, bool final, bool abstrac)
@@ -38,30 +39,35 @@ class SE_Semantic_Analyzer
             else if (type.Contains("->"))
             {
                 main_table[curr_class_name].cdt[key] = new SE_Class_Data_Table(name, type, am, sta, final, abstrac);
+                printCTable();
                 return true;
             }
             else
             {
                 main_table[curr_class_name].cdt[name] = new SE_Class_Data_Table(name, type, am, sta, final, abstrac);
+                printCTable();
                 return true;
-
-            }
+            }   
         }
         return false;
     }
-    public bool insertFuncTable(string name, string type, int scope)
+    public bool insertFuncTable(string name, string type)
     {
-        if (function_table.ContainsKey(name)) return false;
+        if (function_table.ContainsKey(name)) { System.Console.WriteLine("Variable already decleared in Scope"); return false; }
         function_table[name + scope.ToString()] = new SE_Func_Data_Table(name, type, scope);
+        printFTable();
         return true;
     }
     /*     
         look Ups 
     */
-    public SE_Main_Data_Table? lookUpMainTable(string name)
+    public SE_Main_Data_Table? lookUpMainTable(string? name)
     {
-        if (main_table.ContainsKey(name)) return main_table[name];
-        else return null;
+        if (name != null)
+        {
+            if (main_table.ContainsKey(name)) return main_table[name];
+        }
+        return null;
     }
     public SE_Class_Data_Table? lookUpDataTable(string name)
     {
@@ -91,12 +97,12 @@ class SE_Semantic_Analyzer
                 }
             }
         }
-        if (curr_class_name != null) 
-        { 
-            if (main_table[curr_class_name].cdt.ContainsKey(name)) 
+        if (curr_class_name != null)
+        {
+            if (main_table[curr_class_name].cdt.ContainsKey(name))
             {
                 return main_table[curr_class_name].cdt[name].type;
-            } 
+            }
         }
 
         if (global_table.ContainsKey(name)) return global_table[name].type;
@@ -119,5 +125,49 @@ class SE_Semantic_Analyzer
         return null;
     }
     public void createScope() { scope += 1; scopeStack.Add(scope); }
-    public void destroyScope() { scopeStack.RemoveAt(scopeStack.Count - 1); }
+    public void destroyScope()
+    {
+        scopeStack.RemoveAt(scopeStack.Count - 1);
+        if (scopeStack.Count == 0) curr_class_name = "";
+    }
+
+    private void printMainTable()
+    {
+        System.Console.WriteLine("\n\t^^^^SE_Main_Data_Table^^^^");
+        foreach (KeyValuePair<string, SE_Main_Data_Table> kvp in main_table)
+        {
+            System.Console.WriteLine(string.Format("classID = {0}, Value = {1}", kvp.Key, kvp.Value.ToString()));
+        }
+        System.Console.WriteLine("\n");
+    }
+    private void printGTable()
+    {
+        System.Console.WriteLine("\n\t^^^^SE_Global_Data_Table^^^^");
+        foreach (KeyValuePair<string, SE_Global_Data_Table> kvp in global_table)
+        {
+            System.Console.WriteLine(string.Format("ID = {0}, Value = {1}", kvp.Key, kvp.Value.ToString()));
+        }
+        System.Console.WriteLine("\n");
+    }    
+    private void printCTable()
+    {
+        System.Console.WriteLine("\n\t^^^^SE_Class_Data_Table^^^^");
+
+        foreach (KeyValuePair<string, SE_Class_Data_Table> kvp in main_table[curr_class_name].cdt)
+        {
+            System.Console.WriteLine(string.Format("classID = {0}, Value = {1}", kvp.Key, kvp.Value.ToString()));
+        }
+        System.Console.WriteLine("\n");
+    }
+    private void printFTable()
+    {
+        System.Console.WriteLine("\n\t^^^^SE_Function_Data_Table^^^^");
+        
+        // System.Console.WriteLine(function_table.Count);
+        foreach (KeyValuePair<string, SE_Func_Data_Table> kvp in function_table)
+        {
+            System.Console.WriteLine(string.Format("classID = {0}, Value = {1}", kvp.Key, kvp.Value.ToString()));
+        }
+        System.Console.WriteLine("\n");
+    }
 }
